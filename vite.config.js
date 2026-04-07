2cf2951e85a1cd7e2fd7793275d447b989df9816
hank <lohaslife.hank@gmail.com>
2026-04-07 11:24:47 +0800

    修改路徑名稱

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/ltc-dashboard-dev/",
+  base: "/ltc-dashboard/",
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
